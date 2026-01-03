6bc0b20245ecd8cfe7e3e272bacf78b09d081170
fistoon <leblondjul@hotmail.com>
2026-01-03 17:55:43 +0100

    fix: add supabase anon key and improve supabase client initialization

---
+++ test-quill.js
@@ -0,0 +1,6 @@
+const quill = require('quill');
+const reactQuill = require('react-quill');
+
+console.log('Quill version:', quill.default?.version || 'N/A');
+console.log('React-Quill imported successfully');
+console.log('ReactQuill type:', typeof reactQuill.default);
